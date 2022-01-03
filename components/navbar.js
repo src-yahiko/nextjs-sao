c3520c8c04ae129ffa74e56e371573f4ad9b9b02
Omar Bittar <omar.bittar@bitrate-it.de>
2022-01-03 19:27:27 +0100

    barutec style

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,26 @@ export default function MyNavbar({ userData }) {
 
     return <Navbar id="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark" className="pb-2 pt-3 shadow-sm">
         <Container fluid className="px-5">
-            <Link href="/#navbar" passHref><Navbar.Brand className="p-2 px-4  "><div className='d-flex'><Image alt="Baru Logo" height={30} width={24} src="/baru.svg" /><p className="m-0 p-0 px-3">baru tec</p></div></Navbar.Brand></Link>
+            <Link href="/#navbar" passHref>
+                <Navbar.Brand className="">
+                    <div className='d-flex m-0 p-0 align-items-center'>
+                        <div className='pt-2 m-2 align-center-center'>
+                            <Image className='' alt="Baru Logo" height={30} width={24} src="/baru.svg" />
+                        </div>
+                        <div className='m-0 p-0 pt-3'>
+                            <p className='bt m-0 p-0'>baru tec</p>
+                            <p className='ug m-0 p-0 text-secondary'>UG</p>
+                        </div>
+                    </div>
+                    {/* <div className='border d-flex'>
+                        <Image alt="Baru Logo" height={30} width={24} src="/baru.svg" />
+                        <div className='border m-0 p-0 px-3 d-flex flex-column'>
+                            <p className="border p-0 m-0 pt-2">baru tec</p>
+                            <p style={{ fontSize: "0.34em", margin: "10px" }} className="border m-0 p-0 align-self-end">UG</p>
+                        </div>
+                    </div> */}
+                </Navbar.Brand>
+            </Link>
             {/* <Link href="/#navbar" passHref><Navbar.Brand style={{ position: "fixed", backgroundColor: "rgba(0,0,0,0.4);" }} className="p-2 px-4 pr-0 rounded-pill border border-primary"><div className='d-flex'><Image alt="Baru Logo" height={30} width={24} src="/baru.svg" /><p className="m-0 p-0 px-3">baru tec</p></div></Navbar.Brand></Link> */}
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
